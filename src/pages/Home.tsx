e9a554ff5b40a31fed929a23e16077eae61f26cb
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2021-06-23 06:33:44 -0300

    Inicio da tela Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import illustrationImg from '../assets/images/illustration.svg';
+import logoImg          from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
 
 export function Home(){
     return (
@@ -6,7 +8,23 @@ export function Home(){
             <aside>
                 <img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire dúvidas da sua audiência em tempo real</p>
             </aside>
+
+            <main>
+                <img src={logoImg} alt="LetMeAsk" />
+                <button>
+                    <img src={googleIconImg} alt="Logo do Google" />
+                </button>
+                <div>Ou entre numa sala</div>
+                <form>
+                    <input 
+                        type="text" 
+                        placeholder="Digite o código da sala"
+                    />
+                    <button type="submit">Entrar na sala</button>
+                </form>
+            </main>
         </div>
     )
 }
